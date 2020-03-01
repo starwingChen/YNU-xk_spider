8a3bf4a1c1e92be8be24394157e86506f7c5ca95
watch <707491663@qq.com>
2020-03-01 19:47:10 +0800

    ver 2.11 upgrade tips

--- xk_spider/run.py
+++ xk_spider/run.py
@@ -8,21 +8,23 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/80.0.3987.116 Safari/537.36'
 }
-path = ''  # 填写你的chromedriver路径，如 'D:/webdriver/chromedriver.exe'
 url = 'http://xk.ynu.edu.cn/xsxkapp/sys/xsxkapp/*default/index.do'
 stdCode = ''  # 在''中填入你的学号
-pswd = ''  # 填你的密码
-# 如果你有安全上的考虑，上两行不填也行，等浏览器打开了再填也可以
+pswd = ''  # 填你的密码,如果你有安全上的考虑也可以等浏览器打开了再填
 key = ''  # 填你在server酱上获取到的key
+path = ''  # 填写你的chromedriver路径，如 'D:/webdriver/chromedriver.exe'
 
-# 下面这个列表填你想查询的 素选课 ，以 ['课程全称', '授课老师'], 的格式填，注意最后有一个 英文 逗号
+# 下面这个列表填你想查询的 素选课 ，以 ['课程名称', '授课老师'], 的格式填，注意最后有一个 英文 逗号
+
+# -----  注意，课程名称要保证在选课页面你能用这个名称搜得出来 !!!  -----
+
 publicCourses = [
     ['初级泰语', '赵娟'],  # 这是个测试用例，可以先不修改直接运行看看是否成功，如果不小心抢到了自己手动退掉就好
 ]
 
 # 下面这个列表填你想查询的 主修课，包括必修和选修，格式填写同上
 programCourse = [
-    ['课程全称', '授课老师'],  # 格式同上，如果不需要请把 整行 都删掉
+    ['课程名称', '授课老师'],  # 格式同上，如果不需要请把 整行 都删掉
 ]
 
 '''以上两个列表理论上可以接受任意数量的课程，填写模板如下。但数量最好不要超过你CPU的核心数（一般电脑都在4核以上）
